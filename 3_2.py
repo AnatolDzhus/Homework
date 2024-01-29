45c0f694c31d7404bb1b3f63e526f85b5a87f299
AnatolDzhus <dzhusanatol@gmail.com>
2024-01-29 23:06:24 +0300

    add file

---
+++ 3_2.py
@@ -0,0 +1,6 @@
+first = int(input("first number:"))
+second = int(input("second number:"))
+third = int(input("third number:"))
+average = (first + second + third) / 3
+round_average = round(average, 3)
+print(round_average)
