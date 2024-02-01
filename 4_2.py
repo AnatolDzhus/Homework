a8b8d56d55bc006eadb85a71ecd08dd1cea4dd2a
AnatolDzhus <dzhusanatol@gmail.com>
2024-02-01 21:41:47 +0300

    add file

--- 4_2.py
+++ 4_2.py
@@ -1,5 +1,5 @@
 #number of each letter in text
 list_of_letters=list(input("enter text:"))
-nuber_of_letters=[list_of_letters.count(i) for i in list_of_letters]
-dictionary={list_of_letters[i]:nuber_of_letters[i] for i in range(len(list_of_letters))}
+number_of_letters=[list_of_letters.count(i) for i in list_of_letters]
+dictionary={list_of_letters[i]:number_of_letters[i] for i in range(len(list_of_letters))}
 print(dictionary)
