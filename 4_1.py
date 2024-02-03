93fd4d8fd8bb5b8bbfe1c9fe7d017d86f75b71a7
AnatolDzhus <dzhusanatol@gmail.com>
2024-02-03 16:39:19 +0300

    add file

--- 4_1.py
+++ 4_1.py
@@ -1,4 +1,4 @@
-#список от 2^1 до 2^n
-n=int(input("enter n:"))
-list=[2**i for i in range(1,n)]
+# список от 2^1 до 2^n
+n = int(input("enter n:"))
+list = [2 ** i for i in range(1, n)]
 print(list)
