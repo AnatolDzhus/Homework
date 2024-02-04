61bd08ab35693ebea7c266d9f89c0b7b7c749be7
AnatolDzhus <dzhusanatol@gmail.com>
2024-02-04 20:11:44 +0300

    add file

--- 4_3.py
+++ 4_3.py
@@ -1,12 +1,12 @@
-# #Заполнить словарь где ключами будут выступать числа от 0 до n, а
+#Заполнить словарь где ключами будут выступать числа от 0 до n, а
 # значениями вложенный словарь с ключами "name" и "email", а значения
 # для этих ключей будут браться с клавиатуры
-first_value = input("enter first value:")
-second_value = input("enter second value:")
 n=int(input("enter n:"))
-internal_dict = {"name":first_value, "email":second_value}
-# keys_for_external_dict=[i for i in range(n)]
-# total_dict={keys_for_external_dict[i]:internal_dict for i in range(len(keys_for_external_dict))}
-# print(total_dict)
-total_dict={i:internal_dict for i in range(n)}
-print(total_dict)
+list_of_names=[input("name: ") for i in range(3)]
+list_of_emails=[input("email: ")for i in range(n)]
+dictionary={i:{"name":list_of_names[i],"email":list_of_emails[i]} for i in range(n)}
+print(dictionary)
+
+
+
+
