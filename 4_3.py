6037f21131d751b5a3ff598af41f4337db6f1685
AnatolDzhus <dzhusanatol@gmail.com>
2024-02-04 20:13:04 +0300

    add file

--- 4_3.py
+++ 4_3.py
@@ -2,7 +2,7 @@
 # значениями вложенный словарь с ключами "name" и "email", а значения
 # для этих ключей будут браться с клавиатуры
 n=int(input("enter n:"))
-list_of_names=[input("name: ") for i in range(3)]
+list_of_names=[input("name: ") for i in range(n)]
 list_of_emails=[input("email: ")for i in range(n)]
 dictionary={i:{"name":list_of_names[i],"email":list_of_emails[i]} for i in range(n)}
 print(dictionary)
