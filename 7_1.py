6a1e8d4402972146456b9c41ff0500734f984aff
AnatolDzhus <dzhusanatol@gmail.com>
2024-02-17 22:47:55 +0300

    add file

--- 7_1.py
+++ 7_1.py
@@ -1,5 +1,14 @@
+# 1. Написать класс Car
+# Конструктор класса принимает атрибуты:
+# color: str (цвет)
+# count_passenger_seats: int (количество пассажирских мест)
+# is_baby_seat: bool (наличие десткого кресла)
+# is_busy: bool (определяется внутри конструктора со значением False, не принимается на
+# вход)
+# 1.1 Написать магический метод __str__ выводящий форматированную строку с информацией
+# об автомобиле
 class Car:
-    def __init__(self, color:str, count_passenger_seats:int, is_baby_seat:bool, is_busy=False):
+    def __init__(self, color: str, count_passenger_seats: int, is_baby_seat: bool, is_busy=False):
         self.color = color
         self.count_passenger_seats = count_passenger_seats
         self.is_baby_seat = is_baby_seat
